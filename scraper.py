d632a38f7136d22b0e3b1e3e903cb61b7d0ca91c
Quinten Desmyter <quintendesmyter@hotmail.com>
2024-05-19 16:08:21 +0200

    Update scraper.py
    add  month checker to

--- scraper.py
+++ scraper.py
@@ -62,15 +62,33 @@ while True:
     # Close the driver when you're done
     driver.quit()
 
+    # Use regex to extract the day of the date
     day_of_date = re.search(r'\d+', getDate).group()
+    # get only the alphabetic characters from the string getdate put it inside a variable and print it out. use a regular expression for getting the alphabetic characters
+    month_and_day_of_date = re.sub(r'[^a-zA-Z]', '', getDate)
+    
     #check the value if it is smaller then 27  give me an alert also print out the current value)
     print("The day of the date is: " + day_of_date)
 
     title = "Alert!"
     message = "The day of the date is: " + day_of_date
-    
+    # check if the string month_and_day_of_date contains a month
+
+    # Define a list of Dutch month names
+    dutch_months = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december']
+
+    # Check if the string month_and_day_of_date contains a Dutch month print this month out.
+    for month in dutch_months:
+        if month in month_and_day_of_date:
+            print("The month of the date is: " + month)
+            currentMonth = month
+            break
+        else:
+            currentMonth = "No month found"
+
+    # create an if statement with and operator to check if the day of the date is smaller than 27 and the month is equal to the current month
     # Run the main loop to display the window
-    if int(day_of_date) < 27:
+    if int(day_of_date) < 27  and re.search('mei', currentMonth):
         print("The day of the date is smaller than 27")
         root = Tk()
         root.state('zoomed')
@@ -80,6 +98,4 @@ while True:
         label = Label(root, text=message, font=("Arial", 65))
         label.pack()
         root.mainloop()
-
-
     time.sleep(60)
